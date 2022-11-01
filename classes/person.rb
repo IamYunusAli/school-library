f07ef2615055cd7b92e26f07c47564757259f9aa
iamyunusali <aliyunus291@gmail.com>
2022-11-01 14:45:14 +0300

    Edit Person class to include Nameable

--- classes/person.rb
+++ classes/person.rb
@@ -1,12 +1,24 @@
-class Person
+require './nameable'
+require '../decorator/decorator'
+
+class Person < Nameable
   attr_reader :id, :parent_permission
   attr_accessor :name, :age
 
   def initialize(age, name = 'Uknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   private
@@ -14,10 +26,11 @@ class Person
   def of_age?
     @age >= 18
   end
-
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = Capitalize.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = Trimmer.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
