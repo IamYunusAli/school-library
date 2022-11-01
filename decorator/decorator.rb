e3eb3a08d612d79bb97a1194ceaa0e3f6d1e0e5e
iamyunusali <aliyunus291@gmail.com>
2022-11-01 14:50:27 +0300

    fix linter errors

--- decorator/decorator.rb
+++ decorator/decorator.rb
@@ -1,24 +1,24 @@
 require '../classes/nameable'
 
 class Decorator < Nameable
-    def initialize(nameable)
-        @nameable = nameable
-        super()
-    end
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
 
-    def correct_name
-        @nameable.correct_name
-    end
+  def correct_name
+    @nameable.correct_name
+  end
 end
 
 class Capitalize < Decorator
-    def correct_name
-        @nameable.correct_name.capitalize
-    end
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
 end
 
 class Trimmer < Decorator
-    def correct_name
-        @nameable.correct_name[0..9]
-    end
+  def correct_name
+    @nameable.correct_name[0..9]
+  end
 end
