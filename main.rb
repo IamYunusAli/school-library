b89fe03f1f47478b664ad6d8ba0c9a8c6919f38a
iamyunusali <aliyunus291@gmail.com>
2022-11-07 15:53:19 +0300

    remove choice implementation

--- main.rb
+++ main.rb
@@ -1,26 +1,5 @@
 require_relative './app'
 
-def choices(app, input)
-  case input
-  when 1
-    app.list_books
-  when 2
-    app.list_people
-  when 3
-    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-    app.create_a_person
-  when 4
-    app.create_a_book
-  when 5
-    app.create_a_rental
-  when 6
-    puts 'ID of person'
-    app.list_rentals_by_id
-  else
-    puts 'Command not found'
-  end
-end
-
 def run(app)
   puts ['',
         'Please choose an option by entering a number:',
@@ -33,7 +12,6 @@ def run(app)
         '7 - Exit']
   input = gets.chomp.to_i
   return puts 'Thank you for using the school library!' unless input != 7
-
   choices(app, input)
   run(app)
 end
